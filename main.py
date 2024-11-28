999fe079ca72f2367bb7471865e2845d85d34d7d
yrustamjon <149563071+yrustamjon@users.noreply.github.com>
2024-11-28 19:59:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,174 @@
+# print('9-misol')
+# a=int(input('a ni kiriting : '))
+# b=int(input('b ni kiriting : '))
+# print(a==b+19)
+# print('13-MISOL')
+# a=int(input('a ni kiriting : '))
+# birlar=a%10
+# print(birlar==5)
+# a=int(input('4 xonali a ni kiriting : '))
+# minlar=a//1000%10
+# yuzlar=a//100%10
+# onlar=a//10%10
+# birlar=a%10
+# print(minlar+yuzlar+onlar+birlar>=10)
+# print('3-misol')
+# a=int(input('a ni kiriting : '))
+# print(a%3==0 and a%4!=0)
+
+# print('2-misol')
+# a=int(input('a ni kiriting : '))
+# print(a==5**3 or a==5**2 )
+
+# print('4-misol')
+
+# a=int(input(' a 3 xonali sonni kiriting : '))
+# b=int(input(' b 3 xonali sonni kiriting : '))
+
+# asum=a//100%10+a//10%10+a%10
+# bsum=b//100%10+b//10%10+b%10
+# print(asum==bsum)
+
+# print('11-misol')
+# a=int(input('a ni kiriting : '))
+# b=int(input('b ni kiriting : '))
+# print(a+23==b or b+23==a)
+
+# print('5-misol')
+# a=int(input('a 4 xonali sonni kiriting : '))
+# b=int(input('b 4 xonali sonnni kiriting : '))
+# a_sum=a%10+a//10%10+a//100%10+a//1000%10
+# b_sum=b%10+b//10%10+b//100%10+b//1000%10
+# print(a_sum==2*b_sum or b_sum==2*a_sum)
+# print('6-misol')
+# a=int(input('a 4 xonali sonni kiriting : '))
+# b=int(input('b 4 xonali sonnni kiriting : '))
+# a_sum=a%10+a//10%10+a//100%10+a//1000%10
+# b_sum=b%10+b//10%10+b//100%10+b//1000%10
+# print(a_sum==b_sum**2 or b_sum==a_sum**2)
+# print('7-misol')
+# a=int(input('a 4 xonali sonni kiriting : '))
+# b=int(input('b 1 xonali sonnni kiriting : '))
+# a_sum=a%10+a//10%10+a//100%10+a//1000%10
+# print(a_sum/4==b )
+# print('8-misol')
+# a=int(input('a 4 xonali sonni kiriting : '))
+# b=int(input('b 1 xonali sonnni kiriting : '))
+# a_sum=a%10+a//10%10+a//100%10+a//1000%10
+# print(a_sum/4>b )
+# print('12-misol')
+# a=int(input('a ni kiriting: '))
+# b=int(input('b ni kiriting: '))
+# print(a==b*5 or b==a*5)
+# print('14-misol')
+# a=int(input('a ni kiriting: '))
+# birlar=a%10
+# print(birlar==5 or birlar==9)
+# print('15-misol')
+# son=int(input('4 xonali son ni kiriting: '))
+# print(son//1000%10==son%10)
+# print('16-misol')   
+# a=int(input('4 xonali a ni kiriting: '))
+# print(a//1000%10+a%10==a//100%10+a//10%10)
+# print('17-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//100%10==a%10)
+# print('18-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//100%10==a//10%10==a%10)
+# print('19-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//100%10>a//10%10 and a//100%10>a%10)
+# print('20-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//100%10<a%10 and a//10%10<a%10 )
+# print('21-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//100%10>a//10%10<a%10)
+# print('22-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//10%10==4+a%10)
+# print('23-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//10%10+a%10==a//10%10*a%10)
+# print('24-misol')
+# a=int(input('a ni kiriting: '))
+# print(a//1000%10+a%10==a//100%10*a//10%10)
+# print('25-misol')
+# a=int(input('a ni kiriting: '))
+# b=int(input('b ni kiriting: '))
+# c=int(input('c ni kiriting: '))
+# d=int(input('d ni kiriting: '))
+# print(b>a and b>c and b>d and b%2==0)
+# print('26-misol')
+# a=int(input('a ni kiriting: '))
+# b=int(input('b ni kiriting: '))
+# c=int(input('c ni kiriting: '))
+# d=int(input('d ni kiriting: '))
+# a_=a>0
+# b_=b>0
+# c_=c>0
+# d_=d>0
+# k=a_+b_+c_+d_
+# print(k/2==1)
+# print('27-misol')
+# a=int(input('a ni kiriting: '))
+# b=int(input('b ni kiriting: '))
+# c=int(input('c ni kiriting: '))
+# d=int(input('d ni kiriting: '))
+# a_=a>0
+# b_=b>0
+# c_=c>0
+# d_=d>0
+# k=a_+b_+c_+d_
+# print(k/3==1)
+# print('28-misol')
+# a=int(input('5xonali a ni kiriting: '))
+# b=int(input('5xonali b ni kiriting: '))
+# c=int(input('5xonali c ni kiriting: '))
+# a_sum=a//10000%10+a//1000%10+a//100%10+a//10%10+a%10
+# b_sum=b//10000%10+b//1000%10+b//100%10+b//10%10+b%10
+# c_sum=c//10000%10+c//1000%10+c//100%10+c//10%10+c%10
+# print(a_sum==b_sum==c_sum)
+# print('29-misol')
+# a=int(input('3xonali a ni kiriting: '))
+# b=int(input('3xonali b ni kiriting: '))
+# c=int(input('3xonali c ni kiriting: '))
+# a_sum=a//100%10*a//10%10*a%10
+# b_sum=b//100%10*b//10%10*b%10
+# c_sum=c//100%10*c//10%10*c%10
+# print(a_sum==b_sum==c_sum)
+# print('30-misol')
+# a=int(input('3xonali a ni kiriting: '))
+# b=int(input('3xonali b ni kiriting: '))
+# c=int(input('3xonali c ni kiriting: '))
+# d=int(input('3xonali d ni kiriting: '))
+# a_=a==2*b or a==2*d or a==2*c 
+# b_= b==2*c or b==2*d or b==2*a
+# c_= c==2*d or c==2*a or c==2*b
+# d_= d==2*a or d==2*b or d==2*c
+# print(a_ or b_ or c_ or d_)
+# print('31-misol')
+# a=int(input('a ni kiriting: '))
+# b=int(input('b ni kiriting: '))
+# c=int(input('c ni kiriting: '))
+# d=int(input('d ni kiriting: '))
+# print(a%2+b%2+c%2+d%2>=2)
+# print('32-misol')
+# a=int(input('a ni kiriting: '))
+# b=int(input('b ni kiriting: '))
+# c=int(input('c ni kiriting: '))
+# # if a>b:
+# #     if a>c:
+# #         print(a)
+# #     else:
+# #         print(c)
+# # else:
+# #     if b>c:
+# #         print(b)
+# #     else:
+# #         print(c)
+
+# eng_katta = (a + b + abs(a - b)) / 2 
+# eng_katta = (eng_katta + c + abs(eng_katta - c)) / 2  
+# print(f"Eng katta son: {int(eng_katta)}")
